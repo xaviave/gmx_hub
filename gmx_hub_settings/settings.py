0e9e34af25c1ffe7e17ed400c204db724b414507
xaviave <hellxaviave@gmail.com>
2020-11-02 13:02:44 +0100

    default little django site

--- gmx_hub_settings/settings.py
+++ gmx_hub_settings/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # CUSTOM APP
+    "gmx_site.apps.GmxSiteConfig",
 ]
 
 MIDDLEWARE = [
--- gmx_site/views.py
+++ gmx_site/views.py
@@ -1,9 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from django.views import generic
+from .models import Choice, Question
 
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'gmx_site/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    return HttpResponse("Hello world")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'gmx_site/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'gmx_site/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'gmx_site/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('gmx_site:results', args=(question.id,)))
